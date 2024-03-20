87940f817f5d78c800291972b2a9c6df9737d5ff
matthiashippe <matthias@hippe.eu>
2024-03-20 13:18:57 +0100

    added basic sharing redirect

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ src/pages/remote/index.tsx
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Document() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const urlParams = new URLSearchParams(window.location.search);
+    const pageId = urlParams.get("pageId");
+
+    // Check if URL matches the expected format
+    ///share\.mediceo\.com\/remote/.test(window.location.href) &&
+    if (pageId) {
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        // iOS Devices
+        window.location.href = `app.mediceo.com://shared?pageId=${pageId}`;
+      } else if (/android/i.test(userAgent)) {
+        // Android Devices
+        window.location.href = `app.mediceo.com://shared?pageId=${pageId}`;
+      } else {
+        // Web Redirection
+        router.push(`https://app.mediceo.com/shared?pageId=${pageId}`);
+      }
+    }
+  }, []);
+  return <div></div>;
+}
