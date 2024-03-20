d6ec35f4f813dfbb5e1fd4e86e8ffb2de40c9af2
matthiashippe <matthias@hippe.eu>
2024-03-20 13:25:30 +0100

    removed typibg mistakes

--- src/pages/remote/index.tsx
+++ src/pages/remote/index.tsx
@@ -5,14 +5,15 @@ export default function Document() {
   const router = useRouter();
 
   useEffect(() => {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    const userAgent =
+      navigator.userAgent || navigator.vendor || (window as any).opera;
     const urlParams = new URLSearchParams(window.location.search);
     const pageId = urlParams.get("pageId");
 
     // Check if URL matches the expected format
     ///share\.mediceo\.com\/remote/.test(window.location.href) &&
     if (pageId) {
-      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
         // iOS Devices
         window.location.href = `app.mediceo.com://shared?pageId=${pageId}`;
       } else if (/android/i.test(userAgent)) {
