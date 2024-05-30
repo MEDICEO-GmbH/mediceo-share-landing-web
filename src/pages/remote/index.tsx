85ea22ecda445bb786d68501707d3bdf633b5b91
matthiashippe <matthias@hippe.eu>
2024-05-30 09:37:23 +0200

    added caseData optionally

--- src/pages/remote/index.tsx
+++ src/pages/remote/index.tsx
@@ -9,19 +9,28 @@ export default function Document() {
       navigator.userAgent || navigator.vendor || (window as any).opera;
     const urlParams = new URLSearchParams(window.location.search);
     const pageId = urlParams.get("pageId");
+    const caseData = urlParams.get("caseData");
 
     // Check if URL matches the expected format
     ///share\.mediceo\.com\/remote/.test(window.location.href) &&
     if (pageId) {
       if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
         // iOS Devices
-        window.location.href = `com.mediceo.app://shared?pageId=${pageId}`;
+        window.location.href = `com.mediceo.app://shared?pageId=${pageId}${
+          caseData ? `&caseData=${caseData}` : ""
+        }`;
       } else if (/android/i.test(userAgent)) {
         // Android Devices
-        window.location.href = `com.mediceo.app://shared?pageId=${pageId}`;
+        window.location.href = `com.mediceo.app://shared?pageId=${pageId}${
+          caseData ? `&caseData=${caseData}` : ""
+        }`;
       } else {
         // Web Redirection
-        router.push(`https://app.mediceo.com/shared?pageId=${pageId}`);
+        router.push(
+          `https://app.mediceo.com/shared?pageId=${pageId}${
+            caseData ? `&caseData=${caseData}` : ""
+          }`
+        );
       }
       //window.close();
     }
